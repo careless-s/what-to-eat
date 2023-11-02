7b84f06888b180bd87889ad0b7ad68cd283406c2
careless <careless.s.ou@outlook.com>
2023-11-03 00:11:10 +0800

    解决跨域

--- what-to-eat/src/main/java/com/careless/whattoeat/menu/controller/MenuController.java
+++ what-to-eat/src/main/java/com/careless/whattoeat/menu/controller/MenuController.java
@@ -2,16 +2,14 @@ package com.careless.whattoeat.menu.controller;
 
 import com.careless.whattoeat.menu.service.MenuService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
 @AllArgsConstructor
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/menu")
 public class MenuController {
 
